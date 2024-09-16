751a3dd23d0243a6b24ac275dff2df17418141bc
iftykhar alam <s.m.ifty49@gmail.com>
2024-09-16 09:50:49 +0600

    initiating template mastering

---
+++ Assingment-6/resources/views/Theme/nav.blade.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white py-3">
+    <div class="container px-5">
+        <a class="navbar-brand" href="{{ route('about') }}"><span class="fw-bolder text-primary">Iftykhar Alam</span></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
+                <li class="nav-item"><a class="nav-link" href="{{ route('about') }}">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="https://drive.google.com/file/d/12WJX1_2h_i0uWD_GV4njUvRqGR_Wo3hb/view?usp=sharing">Resume</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('project') }}">Projects</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('work') }}">Work Experince</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
