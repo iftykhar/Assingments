5e58805d27ffb41d451a36b5d125f6159fbb941f
iftykhar alam <s.m.ifty49@gmail.com>
2024-10-02 22:36:28 +0600

    validating registration

--- Assingnment-7/app/Http/Controllers/AuthController.php
+++ Assingnment-7/app/Http/Controllers/AuthController.php
@@ -7,4 +7,13 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     //
+
+    public function register(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'username' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirm',
+        ]);
+    }
 }
