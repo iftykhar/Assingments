2a577280aee4ae8b7472ae309961b399a59924c4
iftykhar alam <s.m.ifty49@gmail.com>
2024-10-08 23:54:10 +0600

    registration

--- Assingnment-7/app/Http/Controllers/AuthController.php
+++ Assingnment-7/app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -24,16 +26,29 @@ class AuthController extends Controller
     ]);
 
     // Insert user data into the database using Query Builder
-    $create_user = DB::table('users')->insert([
-        'name' => $validated['name'],
-        'username' => $validated['username'],
-        'email' => $validated['email'],
-        'password' => Hash::make($validated['password']),
-        'created_at' => now(),
-        'updated_at' => now(),
+    // $create_user = DB::table('users')->insert([
+    //     'name' => $validated['name'],
+    //     'username' => $validated['username'],
+    //     'email' => $validated['email'],
+    //     'password' => Hash::make($validated['password']),
+    //     'created_at' => now(),
+    //     'updated_at' => now(),
+    // ]);
+    //     // dd($create_user);
+    //     // return response()->json(['message' => 'User registered successfully!']);
+    //     return view('welcome');
+
+
+    $user = User::create([
+        'name' => $request->name,
+        'username' => $request->username,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
     ]);
-        dd($create_user);
-        return response()->json(['message' => 'User registered successfully!']);
+
+    // Auth::login($user);
+
+    return dd($user);
 
     }
 }
